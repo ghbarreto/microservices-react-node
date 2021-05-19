bfb1864d87802d021971136035f82f28bdcbf621
ivlen <gh.barreto@hotmail.com>
2021-05-18 22:02:27 -0300

    fixed a bug with comments

--- Section2/client/src/PostList.js
+++ Section2/client/src/PostList.js
@@ -33,23 +33,6 @@ const PostList = () => {
     );
   });
 
-  const renderedPosts = Object.values(posts).map(post => {
-    return (
-      <div
-        key={post.id}
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-      >
-        <div className="card-body" style={{ fontSize: "15px" }}>
-          <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
-
-          <CommentCreate postId={post.id} />
-        </div>
-      </div>
-    );
-  });
-
   return (
     <div
       className=" flex-row flex-wrap justify-content-between"
