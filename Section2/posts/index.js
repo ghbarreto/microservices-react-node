b18f6fed50f5bc3f37197ce6faba4acf4f02966e
ivlen <gh.barreto@hotmail.com>
2021-05-22 00:19:29 -0300

    event bus and a query

--- Section2/posts/index.js
+++ Section2/posts/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 // gets rid of the CORS error
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   // creating a new random id
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
@@ -26,7 +27,21 @@ app.post("/posts", (req, res) => {
     title,
   };
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("event received: ", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
